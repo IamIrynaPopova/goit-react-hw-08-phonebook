74edde11cfce0a985760b5ec3c3cc9a107724ccd
Iryna Popova <popovaira969@gmail.com>
2023-04-29 23:44:16 +0300

    add again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, NavLink } from 'react-router-dom';
 import Home from './Home/Home';
 import ContactForm from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
+// import { ContactList } from './ContactList/ContactList';
+// import { Filter } from './Filter/Filter';
 
 export const App = () => {
   return (
